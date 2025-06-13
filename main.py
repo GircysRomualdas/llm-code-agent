9005cb2d0837ed00bddab619dbad0ee86f7964ca
romas <r2udis@gmail.com>
2025-06-13 16:32:36 +0300

    add get files info function schema

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from prompts import system_prompt
+from call_function import available_functions
 
 
 def main():
@@ -29,14 +31,22 @@ def main():
 
 def generate_content(client, messages, verbose):
     response = client.models.generate_content(
-            model="gemini-2.0-flash-001",
-            contents=messages)
-
+        model="gemini-2.0-flash-001",
+        contents=messages,
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=system_prompt
+        ),
+    )
     if verbose:
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
 
-    print(f"Response text: {response.text}")
+    if not response.function_calls:
+        return response.text
 
-if __name__=="__main__":
+    for function_call_part in response.function_calls:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+
+
+if __name__ == "__main__":
     main()
